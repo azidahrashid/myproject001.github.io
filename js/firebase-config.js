47ebf98c1007b0f0477d7b204084c264a41002fa
azidahrashid <turnkeyjie@gmail.com>
2024-08-16 13:19:30 +0800

    edit files js
    firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,4 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +19,4 @@
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+
